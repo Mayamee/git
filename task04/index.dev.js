aa66a5ff2a085a03022a6e24c3e7d5606ef4464d
Mayamee <ponyashcat228@gmail.com>
2023-07-23 19:53:15 +0300

    Renamed commits

--- task04/index.dev.js
+++ task04/index.dev.js
@@ -4,3 +4,10 @@ console.log("make change 1")
 console.log("make change 2")
 console.log("make change 3")
 console.log("make change 4")
+//t5
+console.log("make change 5")
+console.log("make change 6")
+//t5 - another one
+console.log("make change 7")
+console.log("make change 8")
+//t5 - another one
